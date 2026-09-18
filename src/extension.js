c46c5655e35589928d7fcb7c3ca21219551abb24
Djinn Alexio <djinnalexio+gnome@proton.me>
2026-09-18 08:47:45 +0100

    docs: :page_facing_up: use correct license

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2026 Modern Clock for GNOME Contributors
-// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 import Clutter from 'gi://Clutter';
 import Gio from 'gi://Gio';
